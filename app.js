da94a10a985ad9272bed53244b80291edbbaf37a
tanzeemraza <tanzeemraza243@gmail.com>
2020-07-23 20:07:43 +0500

    Now ready to commit

---
+++ app.js
@@ -0,0 +1,369 @@
+// chapter 1
+// alert ("Error! Please Enter a valid Password");
+
+
+
+// var val1 = 15;
+// if (val1 < 16){
+//     alert ("Welcome to Js Land")
+//          }
+
+// chapter 2
+// var name = +prompt ("what is your name");
+// if (name == 14){
+//     alert("johen doe")
+// }else if (name < 14){
+//     alert("your age is 15")
+// }else{
+//     alert("certify")
+// }
+       
+// var a = +prompt ("enter your roll number");
+// if (a == 15){
+//     alert("example@xample.com")
+// }
+    
+// var a = "enter your value";
+// var b = "your value is";
+// var c = +prompt (a , b);
+// var d = a + 12;
+// document.write ("i can write html content throw Javascript:")
+
+
+// Chapter 3
+// var a = 25;
+// var b = 7;
+// var c = a + b;
+// console.log (c);
+
+// var t = +prompt ("enter your shirt price");
+// if (t == 200) {
+//     alert ("your shirt price is high")
+// }else if (t < 200){
+//    alert ("your shirt price is low")
+// }else{
+//     alert ("John doe buy T shirt from clothing store")
+// }
+
+// Chapter 4 
+// var a = 3.22
+// var b = 89%0
+// var c = nameofband
+// var d = home120
+// alert("Sorry, that's incorrect.")
+
+//chapter 5
+// task 1
+// var a = 3;
+// var b = 5;
+// var c = a + b
+// console.log(c)
+
+// task 5
+// document.write ("4" + "x" + "1" + "=" + 4*1 + "<br>")
+// document.write ("4" + "x" + "2" + "=" + 4*2 + "<br>")
+// document.write ("4" + "x" + "3" + "=" + 4*3 + "<br>")
+// document.write ("4" + "x" + "4" + "=" + 4*4 + "<br>")
+// document.write ("4" + "x" + "5" + "=" + 4*5 + "<br>")
+// document.write ("4" + "x" + "6" + "=" + 4*6 + "<br>")
+// document.write ("4" + "x" + "7" + "=" + 4*7 + "<br>")
+// document.write ("4" + "x" + "8" + "=" + 4*8 + "<br>")
+// document.write ("4" + "x" + "9" + "=" + 4*9 + "<br>")
+// document.write ("4" + "x" + "10" + "=" + 4*10 + "<br>")
+
+// task 11
+// var age = +prompt ("enter your age year");
+// if (age == 1992){
+//     alert ("your age is 24"){
+//     }else if (age < 1992 ){
+//         alert("your age is less")
+//     }
+//     else{
+//         alert ("your age is more")
+//     }
+
+// chapter 6 to 9
+// task
+// var english = 100;
+// var math = 100;
+// var urdu = 100;
+// var english = +prompt("enter your mask")
+// if (english >  80 && english < 100 ){
+//     alert("A")
+// }else if (english >  60 && english <  70){
+//     alert("B")
+// }
+// else if (english >  50 && english < 60)
+//     alert("C")
+// else if (english >  40 && english <  50)
+// alert("D")
+// if (english >  33 && english < 40){
+//     alert("E")
+//     {
+//         alert ("you did not match")
+    // chapter 9 to 11
+    // task 1
+    // var cities = ["karachi","hyderabad","islamabad"];
+    // for (var i =0; i < cities.length; i++){
+    //     if (cities[i] === "karachi"){
+    //         alert(cities[i] + "welcome to the city of lights")
+    //     }
+    // }
+// task 2 
+    // var age = +prompt ("enter your age");
+    // var gender = prompt ("enter your Gender");
+    // if (age > 18 || gender == "male"){
+    //     alert ("good morning sir")
+    // }
+    // else {
+    //     alert("good morning Maam")
+    // }
+// task 3 
+//     var color = prompt ("enter your color");
+//     if (color === red){
+//         alert("Must stop")
+//     }
+//     else if (color == yello){
+//         alert("ready to go ")
+//     }
+// else {
+//     alert("Move Now")
+// }
+
+// chapter 12 to 13 
+// task 6
+// var hour = 13;
+// if (hour < 18){
+//     alert("Good day")
+// } 
+    
+// else{
+//     alert("Good evening")
+
+// }chapter 14 to 16
+// task 11
+// var city = ["karachi" , "Islamabad" , "quetta"];
+// var copycity = city.slice ( 1 ,3 )
+// console.log (city)
+// console.log (copycity)
+
+// task 12
+// var city = ["this" , "is" , "my" , "city"];
+// var copycity = city.slice ( 1 , 2  )
+// var copycity = city.slice ( 1 , 3  )
+// console.log (city)
+// console.log (copycity)
+
+// chapter 17 to 20 
+// task 1
+// var a = $_;
+// var b = "1";
+// var c = tooPot
+// alert("this is correct")
+
+// document.getElementById(ab).innerHTML;
+
+
+// Chapter 21  to 25  
+
+
+// Task 1
+// var firstname = [ "Muhammad","Tanzeem" ]
+// var lastname = ["Raza"]
+// for (var i = 0; i < firstname.length; i ++){
+//     for ( var x = 0; x < lastname.length; x++){
+//         console.log(firstname[i] + " " + lastname [x])
+//     }
+// }
+// Task 3 
+// no
+
+// var num = 35.36
+// var round = parseInt(num)
+// console.log(round)
+
+
+
+
+// Task 5
+// var a = "Pakistani";
+// console.log(a.charAt(3))
+
+
+
+// Task 7 
+// var text = "Pakistan is the Biggest country of Asia and HYDERABAD is the 7th beggest city of pakistan"
+// for (var i = 0; i < text.length; i++) {
+//      if (text.slice(i, i + 9) === "HYDERABAD") {
+//      text = text.slice(0, i) + "ISLAMABAD" + text.slice(i + 9);
+//      console.log (text)
+//      }
+//     }
+
+// Task 8
+// var text = "Pakistan is the Biggest country of Asia and HYDERABAD is the 7th beggest city of pakistan"
+// for (var i = 0; i < text.length; i++) {
+//      if (text.slice(i, i + 3) === "and") {
+//      text = text.slice(0, i) + "&" + text.slice(i + 3);
+//      console.log (text)
+//      }
+//     }
+
+// Task 9
+// var text = "Pakistan is the Biggest country of Asia and HYDERABAD is the 472 beggest city of pakistan"
+// for (var i = 0; i < text.length; i++) {
+//      if (text.slice(i, i + 3) === "472"){
+//      text = text.slice(0, i) + "456" + text.slice(i + 3);
+//      console.log (text)
+//      }
+//     }
+    
+// Task 10
+// var bics = prompt ("enter your bics name")
+// var firstchar =bics.slice (0,1)
+// var otherchar = bics.slice (1)
+// firstchar = firstchar.toUpperCase ()
+// otherchar = otherchar.toLowerCase ()
+// var bics = firstchar + otherchar;
+// console.log(bics)
+
+// Task 16 
+
+// var word = prompt ("enter your word");
+// var check = " ";
+// for(var i = word.length - 1; i >=0; i --){
+//     console.log(word [i])
+// }
+
+// Chapter 25 to 30 
+// Task 1
+// var num = 2.5;
+// var round = Math.round(num)
+// console.log(round)
+
+// var num = 3.5;
+// var round = Math.floor(num)
+// console.log(round)
+
+// var num = 3.5;
+// var round = Math.ceil(num)
+// console.log(round)
+
+// Task 2 
+// var num = -3.5;
+// var round = Math.round(num)
+// console.log(round)
+
+// var num = -2.5;
+// var round = Math.floor(num)
+// console.log(round)
+
+// var num = -2.2;
+// var round = Math.ceil(num)
+// console.log(round)
+// chapter 31 to 34 
+// task 1
+// var a = new Date(25);
+// var b = a.toString();
+// // var b = b.slice(0,3)
+// a.getDay(6)
+// a.getMonth(11)
+// console.log(a)
+
+// chapter 31 to 34
+// task 13
+// var dob = new Date ("dec 26 , 1994")
+// var dobmili = dob.getTime();
+// var today = new Date ();
+// var todaymili = today.getTime()
+// console.log(todaymili);
+
+// chapter 35 to 38 
+// task 4 
+// function getnumber (num){
+//     var result = document.getElementById("result");
+//    result.value += num;
+// }
+
+// function clearresult (){
+
+//    var result = document.getElementById("result")
+//    result.value = ""
+// }
+// function getresult (){
+//    var result = document.getElementById ("result")
+//    result.value =eval(result.value)
+// }
+
+// chapter 38 to 48
+// task 1
+// function first(b,a){
+//     alert(b+a)
+// }
+// first(2,3);
+
+// task 3
+// function calc(num1,opr,num2){
+//     if(opr === "+"){
+//         return num1 + num2
+//     }
+//     else if(opr === "-"){
+//         return num1 - num2
+//     } 
+//     else if(opr === "*"){
+//         return num1 * num2
+//     }else{
+//         return "Incorrect Operator!"
+//     }
+// }
+// var result = calc(5,"-",5);
+// var result1 = calc(4,"*",6);
+// var result2 = calc(4,"$",9)
+// console.log(result)
+// console.log(result1)
+// console.log(result2)
+
+// task 7
+// var name = "eu"
+// switch(name){
+//     case "ali"
+//     alert("hello eu")
+//     case"ghouse"
+//     alert("hello eu")
+//     default:
+//    alert ("iu")
+// }
+// task 1 and 2
+// function f00(){
+//     alert("Thank for purchasing")
+// }
+
+// task 4
+// function too(){
+// Image ("img.jpg")
+// }
+
+// function foo(){
+// Image ("image.jpg")
+// }
+
+// // chapter 49 to 52
+// task 1
+// function getname(){
+// var name = document.getElementById("name")
+// alert(name.value)}
+
+
+// task 2
+
+// function readmore(){
+//     var text = "Lorem ipsum dolor sit amet consectetur adipisicing elit Consectetur facere voluptatem magni corrupti dicta porro libero esse optio inventore soluta aspernatur Sunt ut quisquam repellat porro placeat possimus quam optiot"
+//     var pera = document.getElementById("pera")
+//     pera.innerHTML = text
+// }
+// chapter 53 to 58
+// function changeimage(){
+//     var changeimage = document.getElementById("img")
+//     changeimage.src = src.img
+    
+// }
